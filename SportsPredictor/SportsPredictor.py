419e5ad5d6bb6dbcfcf26b56ce6e2630748234d2
Eric Ma <amcire96@gmail.com>
2015-11-22 12:18:56 -0500

    working on storing data as json into file

--- SportsPredictor/SportsPredictor.py
+++ SportsPredictor/SportsPredictor.py
@@ -2,6 +2,15 @@
 import requests
 import pandas as pd
 from collections import defaultdict
+import datetime
+import os
+import json
+
+today = datetime.date.today()
+todayStr = str(today.month) + "/" + str(today.day) + "/" + str(today.year)
+
+
+
 
 teamsPage = requests.get("http://espn.go.com/nba/teams")
 teamsTree = html.fromstring(teamsPage.content)
@@ -28,17 +37,94 @@ position_number_dict = { ", PG" : 1,
         ", C" : 5,
     }
 
-def position_convert(str):
-    return position_number_dict[str]
+team_dict = {
+        "Boston Celtics" : 1,
+        "Brooklyn Nets" : 2,
+        "New York Knicks" : 3,
+        "Philadelphia 76ers" : 4,
+        "Toronto Raptors" : 5,
+        "Chicago Bulls" : 6,
+        "Cleveland Cavaliers" : 7,
+        "Detroit Pistons" : 8,
+        "Indiana Pacers" : 9,
+        "Milwaukee Bucks" : 10,
+        "Atlanta Hawks" : 11,
+        "Charlotte Hornets" : 12,
+        "Miami Heat" : 13,
+        "Orlando Magic" : 14,
+        "Washington Wizards" : 15,
+        "Golden State Warriors" : 16,
+        "Los Angeles Clippers" : 17, 
+        "Los Angeles Lakers" : 18,
+        "Phoenix Suns" : 19,
+        "Sacramento Kings" : 20,
+        "Dallas Mavericks" : 21,
+        "Houston Rockets" : 22,
+        "Memphis Grizzlies" : 23,
+        "New Orleans Pelicans" : 24,
+        "San Antonio Spurs" : 25,
+        "Denver Nuggets" : 26,
+        "Minnesota Timberwolves" : 27,
+        "Oklahoma City Thunder" : 28,
+        "Portland Trail Blazers" : 29,
+        "Utah Jazz" : 30
+    }
+
+monthdict = {
+        "January" : 1,
+        "February" : 2,
+        "March" : 3,
+        "April" : 4,
+        "May" : 5,
+        "June" : 6,
+        "July" : 7,
+        "August" : 8,
+        "September" : 9,
+        "October" : 10,
+        "November" : 11,
+        "December" : 12
+    }
+
+def team_convert(str):
+    return team_dict[str]
+
+def playerStatsConvert(statsList):
+    
+    positionNum = position_number_dict[statsList[0]]
+
+    if("DNP" in statsList[1]):
+        return [positionNum, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+    mins = int(statsList[1])
+    [fgmstr,fgastr] = statsList[2].split("-")
+    fgm = int(fgmstr)
+    fga = int(fgastr)
+
+    [tpmstr,tpastr] = statsList[3].split("-")
+    tpm = int(tpmstr)
+    tpa = int(tpastr)
+
+    [ftmstr,ftastr] = statsList[4].split("-")
+    ftm = int(fgmstr)
+    fta = int(fgastr)
+
+    restOfList = []
+    for i in range(5,15):
+        restOfList.append(int(statsList[i]))
+
+
+    return [positionNum, mins, fgm, fga, tpm, tpa, ftm, fta] + restOfList
 
 def date_time_convert(str):
     [a,b] = str.split(":")
     time = int(a) + float(b[0:2])/60
     
-    [_,date] = str.split("ET, ")
-    return [date,time]
+    [_,dateStr] = str.split("ET, ")
+    [monthDayStr,yearStr] = dateStr.split(", ")
+    [monthStr,dayStr] = monthDayStr.split(" ")
+    return [monthdict[monthStr], int(dayStr), int(yearStr), time]
 
-for gameid in gameids:
+for gameid in sorted(gameids):
     gameBoxScoreURL = "http://espn.go.com/nba/boxscore?gameId=" + gameid;
     boxScorePage = requests.get(gameBoxScoreURL)
     boxScoreTree = html.fromstring(boxScorePage.content)
@@ -56,21 +142,15 @@ for gameid in gameids:
     #print(awayName)
 
 
-    #awayTeamTRPositionXPath = "//table/thead/tr[contains(th/text(),'" + awayName + "')]/preceding-sibling::*"
-    #awayTeamTRPosition = len(boxScoreTree.xpath(awayTeamTRPositionXPath))
-    #print(boxScoreTree.xpath(awayTeamTRPositionXPath))
-    #print(awayTeamTRPosition)
 
-    #homeTeamTRPositionXPath = "//table/thead/tr[contains(th/text(),'" + awayName + "')]/preceding-sibling::*"
-    #homeTeamTRPosition = len(boxScoreTree.xpath(homeTeamTRPositionXPath))
-    #print(boxScoreTree.xpath(homeTeamTRPositionXPath))
-    #print(homeTeamTRPosition)
-
+    [awayTeam] = boxScoreTree.xpath("//table/thead[position()=1]/tr[@class='team-color-strip']/th/text()")
+    awayTeamNum = team_convert(awayTeam)
     awayPlayeridList = boxScoreTree.xpath("//table/tbody[position()=1 or position()=2]/tr[contains(@class,'player-46')]/@class")
     awayPlayeridList = [x.split("player-46-")[1] for x in awayPlayeridList]
     #print(awayPlayeridList)
 
-
+    [homeTeam] = boxScoreTree.xpath("//table/thead[position()=4]/tr[@class='team-color-strip']/th/text()")
+    homeTeamNum = team_convert(homeTeam)
     homePlayeridList = boxScoreTree.xpath("//table/tbody[position()=4 or position()=5]/tr[contains(@class,'player-46')]/@class")
     homePlayeridList = [x.split("player-46-")[1] for x in homePlayeridList]
     #print(homePlayeridList)
@@ -84,10 +164,13 @@ for gameid in gameids:
         xPathString = "//tr[contains(@class,'player-46-" + playerid + "')]/*/text()"
 
         gameStatsList = []
-        gameStatsList += date_time_convert(game_time_info) + [0,scoreDifference] # 0 for away team and score difference is calc away score - home score
+        # stores own team's number and also the opposing team's number
+        # 0 for away team and score difference is calc away score - home score
+        gameStatsList += date_time_convert(game_time_info) + [awayTeamNum, homeTeamNum] + [0,scoreDifference] 
+        
 
         playerStatsList = boxScoreTree.xpath(xPathString)
-        playerStatsList[0] = position_convert(playerStatsList[0])
+        playerStatsList = playerStatsConvert(playerStatsList)
        # print(playerStatsList)
        # playerMap[playerid].append(gameid)
 
@@ -99,16 +182,30 @@ for gameid in gameids:
 
         gameStatsList = []
         
-        gameStatsList += date_time_convert(game_time_info) + [1, -1 * scoreDifference] # 1 for home team and score difference is calc away score - home score
+        # stores own team's number and also the opposing team's number
+        # 1 for home team and score difference is calc away score - home score
+        gameStatsList += date_time_convert(game_time_info) + [homeTeamNum, awayTeamNum] + [1, -1 * scoreDifference]
 
         playerStatsList = boxScoreTree.xpath(xPathString)
-        playerStatsList[0] = position_convert(playerStatsList[0])
+        playerStatsList = playerStatsConvert(playerStatsList)
        # print(playerStatsList)
        # playerMap[playerid].append(gameid)
 
         playerMap[playerid].append(gameStatsList+playerStatsList)
 
+
+
+
+f = open("PlayerStats.txt","w")
+f.write("Last Modified: " + todayStr + "\n")
+f.write(json.dumps(playerMap))
+f.close()
 #print(playerMap)
-for playerid in playerMap.keys():
-    print(playerid)
-    print(playerMap[playerid])
+#f = open(playerDataFilePath, "w")
+#for playerid in playerMap.keys():
+
+    #f.writeline(playerMap[playerid])
+    #print(playerid)
+    #print(playerMap[playerid])
+
+#f.close()
